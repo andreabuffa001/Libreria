83be9619388409cd84a9c282bd60658ee236c1a1
Andrea Buffa <a.buffa001@gmail.com>
2016-11-30 17:48:52 +0100

    Blocco per visualizzare 4 libri random

--- index.php
+++ index.php
@@ -8,7 +8,16 @@
     <!--menu inline-->
 <!--Fine Blocco Menù-->
 <!-- Lista dei libri random-->
-    <!--quattro blocchi con tutti i dati-_>
+    <!--quattro blocchi con tutti i dati-->
+    <div>
+        <!--inswerimento dei dati presi dal db e ripetuti con foreach-->
+        <?php
+$dbh = new PDO('mysql:host=127.0.0.1;dbname=Libreria', $user, $pass);
+        foreach($dbh->query('SELECT autore.nome, autore.cognome, libro.titolo FROM libro INNER JOIN libro.id_autore = autore.id_autore ORDER BY RAND() LIMIT 4') as $row) {
+        echo '<p>'.$row['libro.titolo'].'</p><br><p>'.$row['autore.nome'].' '.$row['autore.cognome'].'</p>';
+        }
+            ?>
+    </div>
 <!--Fine random-->
 <!--Ultimi Libri Letti-->
     <!--Stile sidebar con copertina titolo e autore-->
