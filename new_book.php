dc561e25a2d33f56bb2284ff40a86f6f4571e611
Andrea Buffa <a.buffa001@gmail.com>
2016-12-01 16:16:08 +0100

    Scheletro form inserimento nuovo libro + Effettuare test su caricamento libro

--- new_book.php
+++ new_book.php
@@ -1,7 +1,11 @@
 <!doctype html>
 <htmL>
 <head>
-
+<style>
+    #note{
+        height: 100px;
+    }
+</style>
 </head>
 <body>
 <!--menu inline-->
@@ -9,8 +13,9 @@
 <!--Blocco inserimento nuovo libro-->
 <div>
     <!--attivazione del caricamento del nuovo libro-->
+    <!--Blocco caricamento libro sul db-->
     <?php
-    if ((isset($_POST["submit"]))) {
+    if ((isset($_POST["carica"]))) {
         $autore_libro= $_POST['autore'];
         $titolo_libro=$_POST['titolo'];
         $genere_libro= $_POST['genere'];
@@ -23,15 +28,74 @@
         try {
             foreach($dbh->query('INSERT INTO `libro`( `id_autore`, `titolo`, `prezzo`, `id_genere`, `anno`, `note`, `id_negozio`, `copertina`) VALUES ("'.$autore_libro.'","'.$titolo_libro.'",'.$prezzo_libro.',"'.$genere_libro.'","'.$anno_libro.'", "'.$note.'","'.$negozio.'","'.$copertina_libro.'")') as $row) {
                 print_r($row);
+                echo "Abbiamo inserito: ".$row['titolo']." di ".$row['nome'].' '.$row['cognome'];
             }
             $dbh = null;
         } catch (PDOException $e) {
             print "Error!: " . $e->getMessage() . "<br/>";
             die();
         }
-    };
+    }
+    Else{
     ?>
-    
+        <!--Form Inserimento nuovo libro-->
+        <form method="post" action="<?php $PHP_SELF ?>" id="form">
+            <table>
+                <tr>
+                    <td>Autore:</td>
+                    <td><input type="text" name="autore"></td>
+                </tr>
+                <tr>
+                    <td>Titolo:</td>
+                    <td><input type="text" name="titolo"></td>
+                </tr>
+                <tr>
+                    <td>Genere</td>
+                    <td><select name="genere">
+                            <optgroup label="Forma 1">
+                                <option value="1">Romanzo</option>
+                                <option value="2">Giallo</option>
+                                <option value="3">Poesia</option>
+                            </optgroup>
+                            <optgroup label="Forma 2">
+                                <option value="4">Poesia</option>
+                                <option value="5">Epico</option>
+                                <option value="6">Dramma</option>
+                            </optgroup>
+                        </select></td>
+                </tr>
+                <tr>
+                    <td>Negozio</td>
+                    <td><select name="negozio">
+                            <option value="1">Feltrinelli (Via Roma)</option>
+                            <option value="2">Mondadori</option>
+                        </select></td>
+                </tr>
+                <tr>
+                    <td>Prezzo:</td>
+                    <td><input type="text" name="prezzo"></td>
+                </tr>
+                <tr>
+                    <td>Anno:</td>
+                    <td><input type="text" name="anno"></td>
+                </tr>
+                <tr>
+                    <td>Note:</td>
+                    <td><input type="text" name="note" id="note"></td>
+                </tr>
+                <tr>
+                    <td><input type="submit" value="carica"></td>
+                </tr>
+            </table>
+        </form>
+        <!--Fine Form INserimento nuovo libro-->
+
+        <?php
+}
+?>
+
+    <!--fine blocco caricamento-->
+
 </div>
 <!--fine blocco di inserimento-->
 
