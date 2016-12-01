098357f8870a72f64979b08e87b2ad764dd19e63
Andrea Buffa <a.buffa001@gmail.com>
2016-12-01 13:00:43 +0100

    Correzione Query SELECT + Correzione Query INSERT (Error #1054 phpmyadmin)

--- index.php
+++ index.php
@@ -6,17 +6,33 @@
 <body>
 <!--Blocco dedicato al menù-->
     <!--menu inline-->
+    <?php
+    $autore_libro= 1;
+    $titolo_libro="Prova libro 2";
+    $genere_libro= 1;
+    $negozio= 1;
+    $dbh = new PDO('mysql:host=127.0.0.1;dbname=libreria', 'root', '');
+        try {
+            foreach($dbh->query('INSERT INTO `libro`( `id_autore`, `titolo`, `prezzo`, `id_genere`, `anno`, `id_negozio`, `copertina`) VALUES ("'.$autore_libro.'","'.$titolo_libro.'",4.49,"'.$genere_libro.'",2016,"'.$negozio.'","copertina.jpg")') as $row) {
+                print_r($row);
+            }
+            $dbh = null;
+        } catch (PDOException $e) {
+            print "Error!: " . $e->getMessage() . "<br/>";
+            die();
+        }
+    ?>
 <!--Fine Blocco Menù-->
 <!-- Lista dei libri random-->
     <!--quattro blocchi con tutti i dati-->
     <div>
         <!--inswerimento dei dati presi dal db e ripetuti con foreach-->
         <?php
-$dbh = new PDO('mysql:host=127.0.0.1;dbname=Libreria', $user, $pass);
-        foreach($dbh->query('SELECT autore.nome, autore.cognome, libro.titolo FROM libro INNER JOIN libro.id_autore = autore.id_autore ORDER BY RAND() LIMIT 4') as $row) {
-        echo '<p>'.$row['libro.titolo'].'</p><br><p>'.$row['autore.nome'].' '.$row['autore.cognome'].'</p>';
-        }
-            ?>
+            $dbh = new PDO('mysql:host=127.0.0.1;dbname=libreria', 'root', '');
+            foreach($dbh->query('SELECT autore.nome, autore.cognome, libro.titolo FROM `libro`,`autore` INNER JOIN libro as libro2 ON autore.idautore = `id_autore` ORDER BY rand() LIMIT 4') as $row) {
+                echo '<p>'.$row['titolo'].' - '.$row['nome'].' '.$row['cognome'].'</p>';
+             }
+        ?>
     </div>
 <!--Fine random-->
 <!--Ultimi Libri Letti-->
