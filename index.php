e2de267a9f26c5728893b58d40db36442d8df736
andrea <a.buffa001@gmail.com>
2016-12-03 12:59:59 +0100

    Creazione regole di stile + Completamento scheletro index

--- index.php
+++ index.php
@@ -2,30 +2,50 @@
     <html>
 <head>
 <!--Mantenere style responsive-->
+    <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
-<!--Blocco dedicato al menù-->
+<div id="conteiner">
+    <!--Blocco dedicato al menù-->
     <!--menu inline-->
-    <ul>
-        <li>Libreria Completa</li>
-        <li>Ricerca</li>
-        <li><a href="new_book.php">Nuovo</a></li>
-    </ul>
-<!--Fine Blocco Menù-->
-<!-- Lista dei libri random-->
-    <!--quattro blocchi con tutti i dati-->
-    <div>
-        <!--inswerimento dei dati presi dal db e ripetuti con foreach-->
+    <div id="menu">
+        <ul>
+            <li>Libreria Completa</li>
+            <li>Ricerca</li>
+            <li><a href="new_book.php">Nuovo</a></li>
+        </ul>
+    </div>
+    <!--Fine Blocco Menù-->
+    <!--Ultimi Libri inseriti-->
+    <!--Stile sidebar con copertina titolo e autore-->
+    <div id="sidebar">
+        <p>Ultimi libri inseriti</p>
+        <!--query select  per ultimi libri inseriti-->
         <?php
-            $dbh = new PDO('mysql:host=127.0.0.1;dbname=libreria', 'root', '');
-            foreach($dbh->query('SELECT autore.nome, autore.cognome, libro.titolo FROM `libro`,`autore` INNER JOIN libro as libro2 ON autore.idautore = `id_autore` ORDER BY rand() LIMIT 4') as $row) {
-                echo '<p>'.$row['titolo'].' - '.$row['nome'].' '.$row['cognome'].'</p>';
-             }
+        $dbh = new PDO('mysql:host=127.0.0.1;dbname=Libreria', 'root', '');
+        foreach($dbh->query('SELECT autore.nome, autore.cognome, libro.titolo FROM `libro`,`autore` INNER JOIN libro as libro2 ON autore.idautore = `id_autore` ORDER BY libro.id_libro DESC LIMIT 4') as $row) {
+            echo '<p>'.$row['titolo'].' - '.$row['nome'].' '.$row['cognome'].'</p>';
+        }
         ?>
     </div>
-<!--Fine random-->
-<!--Ultimi Libri Letti-->
-    <!--Stile sidebar con copertina titolo e autore-->
-<!--Fine ultimi libri letti-->
+    <!--Fine ultimi libri inseriti-->
+    <!-- Lista dei libri random-->
+    <!--quattro blocchi con tutti i dati-->
+    <div id="elenco">
+        <!--stampa dei dati presi dal db e ripetuti con foreach-->
+        <?php
+        $dbh = new PDO('mysql:host=127.0.0.1;dbname=Libreria', 'root', '');
+        foreach($dbh->query('SELECT autore.nome, autore.cognome, libro.titolo FROM `libro`,`autore` INNER JOIN libro as libro2 ON autore.idautore = `id_autore` ORDER BY rand() LIMIT 2') as $row) {
+            echo '<div class="show-book">'.$row['titolo'].'<br>'.$row['nome'].' '.$row['cognome'].'</div>';
+        }?>
+        <br>
+        <?php
+        foreach($dbh->query('SELECT autore.nome, autore.cognome, libro.titolo FROM `libro`,`autore` INNER JOIN libro as libro2 ON autore.idautore = `id_autore` ORDER BY rand() LIMIT 2') as $row) {
+            echo '<div class="show-book">'.$row['titolo'].'<br>'.$row['nome'].' '.$row['cognome'].'</div>';
+        }
+        ?>
+    </div>
+    <!--Fine random-->
+</div>
 </body>
 </html>
