8d39d8a2747dc88e4918866e373c793882fd913f
Andrea Buffa <a.buffa001@gmail.com>
2016-12-06 17:28:25 +0100

    Creazione pagina per la ricerca

---
+++ search.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html>
+<head>
+    <!--Mantenere style responsive-->
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<div id="conteiner">
+    <!--Blocco dedicato al menù-->
+    <!--menu inline-->
+    <div id="menu">
+        <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="all_book.php">Libreria Completa</a></li>
+            <li><a href="search.php">Ricerca</a></li>
+            <li><a href="new_book.php">Nuovo</a></li>
+        </ul>
+    </div>
+    <!--Fine Blocco Menù-->
+    <H2>Ricerca</H2>
