ba62c2a6bc337d6875ca81bc13838f831c5ec062
Andrea Buffa <a.buffa001@gmail.com>
2016-12-07 14:52:19 +0100

    Sistemazione nuove regole di stile

--- index.php
+++ index.php
@@ -17,16 +17,15 @@
         </ul>
     </div>
     <!--Fine Blocco Menù-->
-    <H2>Home</H2>
+    <div id="titoli"><h2>Home</h2><h3>Ultimi libri inseriti</h3></div>
     <!--Ultimi Libri inseriti-->
     <!--Stile sidebar con copertina titolo e autore-->
     <div id="sidebar">
-        <p>Ultimi libri inseriti</p>
         <!--query select  per ultimi libri inseriti-->
         <?php
         $dbh = new PDO('mysql:host=127.0.0.1;dbname=libreria', 'root', '');
-        foreach($dbh->query('SELECT autore.nome, autore.cognome, libro.titolo, libro.copertina FROM `libro`,`autore` INNER JOIN libro as libro2 ON autore.idautore = `id_autore` ORDER BY libro.id_libro DESC LIMIT 4') as $row) {
-            echo '<div><p>'.$row['titolo'].' - '.$row['nome'].' '.$row['cognome'].'</p><br><img src="http://127.0.0.1/uploads/'.($row['copertina']).'" width="100" height="150"> </div>';
+        foreach($dbh->query('SELECT DISTINCT autore.nome, autore.cognome, libro.titolo, libro.copertina FROM `libro`,`autore` INNER JOIN libro as libro2 ON autore.idautore = `id_autore` ORDER BY libro.id_libro DESC LIMIT 4') as $row) {
+            echo '<div class="book-sidebar"><p id="p-libro">'.$row['titolo'].' - '.$row['nome'].' '.$row['cognome'].'</p><br><img id="img-sidebar" src="http://127.0.0.1/uploads/'.($row['copertina']).'" width="100" height="150"> </div>';
         }
         ?>
     </div>
@@ -37,15 +36,9 @@
         <!--stampa dei dati presi dal db e ripetuti con foreach-->
         <?php
         $dbh = new PDO('mysql:host=127.0.0.1;dbname=Libreria', 'root', '');
-        foreach($dbh->query('SELECT autore.nome, autore.cognome, libro.titolo, libro.copertina FROM `libro`,`autore` INNER JOIN libro as libro2 ON autore.idautore = `id_autore` ORDER BY rand() LIMIT 2') as $row) {
-            echo '<div class="show-book">'.$row['titolo'].'<br>'.$row['nome'].' '.$row['cognome'].' <img src="http://127.0.0.1/uploads/'.($row['copertina']).'" width="200" height="250"></div>';
+        foreach($dbh->query('SELECT DISTINCT autore.nome, autore.cognome, libro.titolo, libro.copertina FROM `libro`,`autore` INNER JOIN libro as libro2 ON autore.idautore = `id_autore` ORDER BY rand() LIMIT 4') as $row) {
+            echo '<div class="show-book"><p id="p-libro">'.$row['titolo'].' - '.$row['nome'].' '.$row['cognome'].'</p><img id="img-elenco" src="http://127.0.0.1/uploads/'.($row['copertina']).'" width="200" height="250"></div>';
         }?>
-        <br>
-        <?php
-            foreach($dbh->query('SELECT autore.nome, autore.cognome, libro.titolo, libro.copertina FROM `libro`,`autore` INNER JOIN libro as libro2 ON autore.idautore = `id_autore` ORDER BY rand() LIMIT 2') as $row) {
-            echo '<div class="show-book">'.$row['titolo'].'<br>'.$row['nome'].' '.$row['cognome'].'<img src="http://127.0.0.1/uploads/'.($row['copertina']).'" width="200" height="250"></div>';
-        }
-        ?>
     </div>
     <!--Fine random-->
 </div>
